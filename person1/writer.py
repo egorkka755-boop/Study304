8f2b98ff4fd63ce9895bbe8ec60db31c52fdb8fc
egor <egorkka755@gmail.com>
2025-10-19 15:29:24 +0300

    Второе изменение

--- person1/writer.py
+++ person1/writer.py
@@ -1,7 +1,8 @@
 
 
 def create_file():
-    text = """Привет! Это текстовый файл"""
+    text = """Привет! Это текстовый файл
+    Тут хранится теперь новая информация для person2"""
 
     with open("text_file.txt", "w", encoding="utf-8") as f:
         f.write(text)
