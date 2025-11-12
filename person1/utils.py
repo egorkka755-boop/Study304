b05f61d99b8a2e7918b53b942ef4662b00e37726
egor <egorkka755@gmail.com>
2025-11-12 23:27:43 +0300

    Squash merge: все утилиты из feature-squash объединены в один коммит

--- person1/utils.py
+++ person1/utils.py
@@ -1,2 +1,16 @@
+
 def main_helper():
     return "Главная функция из main"
+
+def helper_function():
+    return "Первый коммит в feature-squash"
+
+def calculate_sum(a, b):
+    return a + b
+
+def multiply_numbers(x, y):
+    return x * y
+
+def is_even(number):
+    return number % 2 == 0
+
